4ebce63b4beaf3b33970e06a8972ca73e7b47c17
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-06-09 00:38:34 -0500

    Basic system. Bootstrap submodule, simple user system

--- HoradeLeer/settings.py
+++ HoradeLeer/settings.py
@@ -1,3 +1,4 @@
+# encoding:utf-8
 """
 Django settings for HoradeLeer project.
 
@@ -11,10 +12,18 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_NAME = "Hora de Leer"
 
+ADMINS = (
+    ('Mauricio Aizaga', 'mauricioaizaga@gmail.com'),
+)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+APPS = tuple()
+folder_apps = ["apps"]
+
+for app in folder_apps:
+    APPS += tuple([app+"."+x for x in os.listdir(os.sep.join([BASE_DIR,app])) if os.path.isdir(os.sep.join([BASE_DIR,app,x]))])
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'h5it^$6^k_-d*ndx$ve-25f3@&=f^!$ufp#3yo=hbp7nbx-7o#'
@@ -36,7 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-)
+) + APPS
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -55,19 +64,18 @@ WSGI_APPLICATION = 'HoradeLeer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+try:
+    from .local_settings import DATABASES
+except ImportError:
+    DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3','NAME': os.path.join(BASE_DIR, 'db.sqlite3'),'USER': '','PASSWORD': '','HOST': '','PORT': '',}}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-co'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
@@ -80,3 +88,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR , 'public/static').replace('\\','/'),
+)
+
+
+LOCALE_PATHS = tuple([os.sep.join([BASE_DIR,APP.replace('.',os.sep),'locale']) for APP in APPS])
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = "/"
--- HoradeLeer/urls.py
+++ HoradeLeer/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'HoradeLeer.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'apps.website.views.home', name='home'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
 )
